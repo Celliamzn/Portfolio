9b1fa9667a8c84cdcd9374428cd49c20843e615e
Cellia Mouzon <mouzoncellia@gmail.com>
2025-02-25 21:20:10 +0100

    test fond

--- src/components/Accueil.jsx
+++ src/components/Accueil.jsx
@@ -4,7 +4,7 @@ import backgroundAccueil from '../assets/images/pexels-sohi-807598.jpg'
 
 function Accueil() {
   return (
-    <div className="section " style={{backgroundImage : `url(${backgroundAccueil}`}} id='home'>
+    <div className="section" style={{backgroundImage : `url(${backgroundAccueil}`}} id='home'>
      
       <h1>Cellia Mouzon</h1>
     </div>
--- src/components/Présentation.jsx
+++ src/components/Présentation.jsx
@@ -1,11 +1,12 @@
 import Cellia from '../assets/images/Cellia.webp'
 import '../styles/presentation.css'
 import CarouselLogos from './CarouselLogos';
+import backgroundPrésentation from "../assets/images/pexels-george-dolgikh-551816-1303092.jpg"
 
 
 function Présentation() {
   return (
-    <div className="section presentation" id="aboutme">
+    <div className="section presentation" style={{backgroundImage : `url(${backgroundPrésentation}`}} id="aboutme">
       <h2 className="presentation--titre">À propos</h2>
       <div className="presentation--paraetimg">
         <p className="presentation--paragraphe">
