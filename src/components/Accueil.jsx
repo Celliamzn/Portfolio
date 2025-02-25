2e398815be6bfbef7715e04c5f81e2a1cf6958e9
Cellia Mouzon <mouzoncellia@gmail.com>
2025-02-25 21:02:52 +0100

    test correction fond

--- src/components/Accueil.jsx
+++ src/components/Accueil.jsx
@@ -1,9 +1,10 @@
 
 import '../styles/Accueil.css'
+import backgroundAccueil from '../assets/images/pexels-sohi-807598.jpg'
 
 function Accueil() {
   return (
-    <div className="section accueil" id='home'>
+    <div className="section " style={{backgroundImage : `url(${backgroundAccueil}`}} id='home'>
      
       <h1>Cellia Mouzon</h1>
     </div>
